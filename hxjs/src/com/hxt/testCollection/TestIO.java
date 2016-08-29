99b3e4a87078354c758b49776f846a077afe7172
hwin <Administrator@PC201604260958>
2016-08-29 16:02:02 +0800

    test

---
+++ hxjs/src/com/hxt/testCollection/TestIO.java
@@ -0,0 +1,12 @@
+package com.hxt.testCollection;
+
+import java.io.File;
+
+public class TestIO {
+	public static void main(String[]args){
+		
+	}
+	public static void readFileByBytes(String fileName){
+		File file = new File(fileName);
+	}
+}
